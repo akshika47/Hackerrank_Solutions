607df57922d5b9644abae81abb80b25e037de9f3
Akshika47 <akshikaakalanka@gmail.com>
2019-04-26 10:24:54 +0100

    Refactored String and Basic Data Strucures

---
+++ Python/Strings/String_Split_and_Join/Solution.py
@@ -0,0 +1,5 @@
+def split_and_join(line):
+    temp = list(line.split())
+    temp = "-".join(temp)
+    return temp
+
---
+++ Python/Strings/What's_Your_Name?/Solution.py
@@ -0,0 +1,4 @@
+def print_full_name(a, b):
+    a = a+" "+b+"!"
+    print("Hello",a,"You just delved into python.")
+
